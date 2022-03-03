8014d70c1c8a14ccd7567625cb8b80cf45c46e32
shinopai <harbor.shinozaki@gmail.com>
2022-03-03 07:28:04 +0000

    alter seeds.rb for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,25 +17,25 @@ parkings = [
   30
 ]
 
-# 50.times do
-#   Shop.create!(
-#     shop_name: Faker::Name.name,
-#     address: '東京都阪神区阪神3-3-4',
-#     shop_image:File.open("./public/images/#{images[rand(0..6)]}"),
-#     parking: parkings[rand(0..5)]
-#   )
-# end
+50.times do
+  Shop.create!(
+    shop_name: Faker::Name.name,
+    address: '東京都阪神区阪神3-3-4',
+    shop_image:File.open("./public/images/#{images[rand(0..6)]}"),
+    parking: parkings[rand(0..5)]
+  )
+end
 
 10.times do
   ShopCategory.create!(
-    shop_id: rand(11.. 60),
+    shop_id: rand(11.. 50),
     category_id: rand(1..7)
   )
 end
 
 10.times do
   ShopPayment.create!(
-    shop_id: rand(11.. 60),
+    shop_id: rand(11.. 50),
     payment_id: rand(1..6)
   )
 end
